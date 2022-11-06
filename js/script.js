f690cb2f1460492b371c5af61f5b2c2b46d67b9a
MicheleRai <rainonemichele1999@gmail.com>
2022-11-06 22:16:29 +0100

    some function added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,65 @@
 /*Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
+
+let counter = 5;
+let arrNum;
+let guessNum;
+let score;
+
+function getRandomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function generateNum(min, max) {
+	const arrRandoms = [];
+	for (let i = 0; i < 5; i++) {
+		do {
+			randomNumber = getRandomInteger(min, max);
+		} while (arrRandoms.includes(randomNumber))
+		arrRandoms.push(randomNumber);
+	}
+	return arrRandoms;
+}
+
+function generateGuess() {
+	const arrGuess = [];
+	for (let i = 0; i < 5; i++) {
+		do {
+			randomNumber = parseInt(prompt('inserisci i numeri che ti ricordi'));
+		} while (arrGuess.includes(randomNumber))
+		arrGuess.push(randomNumber);
+	}
+	return arrGuess;
+}
+
+function checkAllNum(group1, group2) {
+	score = 0
+	for (let i = 0; i < group1.length; i++) {
+		if (group1.includes(group2)) {
+			score++
+		}
+	}
+}
+
+arrNum = generateNum(0, 100);
+console.log(arrNum);
+
+
+const idInterval = setInterval(count, 500);
+function count() {
+	if (counter === 0) {
+		console.log('Inserisci i numeri che ricordi!');
+
+        guessNum = generateGuess();
+        console.log(guessNum);
+
+        score = checkAllNum(guessNum, arrNum)
+        console.log(score);
+
+		clearInterval(idInterval);
+	} else {
+		console.log(counter);
+		counter--;
+	}
+}
